876539885ac144fd16aac8c2cbb764b805b03fa2
Bishal Singh <meh.bishal@gmail.com>
2024-11-17 17:40:56 +0200

    some changes

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -9,7 +9,7 @@ window.Echo = new Echo({
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 6100,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 6100,
-    forceTLS: true,
+    forceTLS: false,
     disableStats: true,
     encrypted: true
 });
